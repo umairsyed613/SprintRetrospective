8387e9bdc6a70df9a9be0ecf37e15453ae16dea8
Umair Syed <naqvi.umair@gmail.com>
2021-10-21 22:00:11 +0200

    new client

---
+++ Src/Client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.sprintretrospective',
+  appName: 'SprintRetrospective',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
--- Src/Client/src/app/splash/splash.module.ts
+++ Src/Client/src/app/splash/splash.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { FolderPageRoutingModule } from './folder-routing.module';
+import { SplashPageRoutingModule } from './splash-routing.module';
 
-import { FolderPage } from './folder.page';
+import { SplashPage } from './splash.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    SplashPageRoutingModule
   ],
-  declarations: [FolderPage]
+  declarations: [SplashPage]
 })
-export class FolderPageModule {}
+export class SplashPageModule {}
